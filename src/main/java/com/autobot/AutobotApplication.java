8fa6804f0610a505afd76eb4517376b3168e08a5
Noel john paul <noeljpaul007@gmail.com>
2026-09-22 04:46:53 +0530

    Initial commit

---
+++ src/main/java/com/autobot/AutobotApplication.java
@@ -0,0 +1,13 @@
+package com.autobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutobotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autobot/AutobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
